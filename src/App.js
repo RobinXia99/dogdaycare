26cab5ac3ec993b9ee20225105d8c18fdd3d01bd
RobinXia99 <>
2022-04-08 03:01:43 +0200

    New Dog Swiper

--- src/App.js
+++ src/App.js
@@ -1,18 +1,34 @@
+import { useEffect } from 'react';
+import { HashRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css';
+import Owner from './classes/owner';
 import NavigationBar from './components/universal/navbar';
 import DaycareInfo from './components/welcome/aboutus';
 import NewDogs from './components/welcome/newdogs';
 import Poster from './components/welcome/poster';
 
 function App() {
+
+  useEffect(() => {
+
+    let owner = new Owner('Robin','Xia','073613125',['1','2','3']);
+
+    console.log(owner);
+
+  },[]);
+
+
+
   return (
     <div className="App">
       <NavigationBar></NavigationBar>
+
       <Poster></Poster>
       <div id='main-wrap'>
         <DaycareInfo></DaycareInfo>
         <NewDogs></NewDogs>
       </div>
+
       
     </div>
   );
--- src/components/welcome/newdogs.js
+++ src/components/welcome/newdogs.js
@@ -1,13 +1,100 @@
-import React from "react";
+import React, { useState } from "react";
+import { useParams } from "react-router-dom";
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
 import '../../styles/welcome.css';
+import '../../styles/fa-icon.css';
+import '../../styles/dogslides.css';
+
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faTag } from "@fortawesome/free-solid-svg-icons";
+import { faPaw } from "@fortawesome/free-solid-svg-icons";
+
+import { faChevronLeft } from "@fortawesome/free-solid-svg-icons";
+import { faChevronRight } from "@fortawesome/free-solid-svg-icons";
 
 const NewDogs = () => {
 
 
+    let { chipNr } = useParams();
+    let dog;
+
+    const [my_swiper, set_my_swiper] = useState({});
+
+    
 
     return (
         <div className="new_dogs_container">
             <h3 className="new_dogs_title">MEET OUR NEWEST FRIENDS</h3>
+            
+            <div className="slides_container">
+
+                <article className="slide_button" onClick={() => { my_swiper.slidePrev() }}>
+                    <FontAwesomeIcon icon={faChevronLeft}/>
+                </article>
+
+            <div className="swiper_holder">
+                <Swiper
+                slidesPerView={1}
+                spaceBetween={50}
+                onInit={(ev) => {
+                    set_my_swiper(ev)
+                }}
+                >
+
+                <SwiperSlide> <DogSlide dogs={'https://images.dog.ceo/breeds/pomeranian/n02112018_8161.jpg'}/> </SwiperSlide>
+                <SwiperSlide> <DogSlide dogs={'https://images.dog.ceo/breeds/akita/Akita_inu_blanc.jpg'}/> </SwiperSlide>
+                <SwiperSlide> <DogSlide dogs={'https://images.dog.ceo/breeds/akita/Akita_inu_blanc.jpg'}/> </SwiperSlide>
+
+            </Swiper>
+            </div>
+            
+            <article className="slide_button" onClick={() => { my_swiper.slideNext() }}>
+                <FontAwesomeIcon icon={faChevronRight}/>
+            </article>
+
+
+            </div>
+            
+            
+        </div>
+    )
+}
+
+const DogSlide = ({dogs}) => {
+
+
+    return (
+
+        <div className="dog_image_outer_holder">
+            <div className="dog_image_holder">
+            <h3 className="woof">WOOF</h3>
+
+            <img className="dog_image" src={dogs}/>
+
+            <DogTags></DogTags>
+
+            </div>
+        </div>
+        
+    )
+
+
+}
+
+const DogTags = (dog) => {
+
+    return (
+        <div className="dog_tag_container">
+            <article>
+                <FontAwesomeIcon className="fa_icon_dogtag" icon={ faTag } />
+                <h3>Wilma</h3>
+            </article>
+
+            <article>
+                <FontAwesomeIcon className="fa_icon_dogtag" icon={ faPaw } />
+                <h3>Akita</h3>
+            </article>
         </div>
     )
 }
